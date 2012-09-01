4092ad32f8189d25d8cc6a25631ae40c97d0279e
ShaddiX <web@shadx.net>
2012-09-01 17:16:39 +0300

    Центральный слайдер товаров на главной

---
+++ js/init.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("#scroll-1").jcarousel({
+    wrap: 'circular',
+    auto: 0
+  });
+  
+  $("#scroll-2").jcarousel({
+    wrap: 'circular',
+    auto: 0
+  });
+});
